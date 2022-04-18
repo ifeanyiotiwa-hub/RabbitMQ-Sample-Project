131b4105213cc5397e214fc1c45cb883a08bf1f2
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-04-18 10:50:50 +0100

    for multiple consumer consuming messages and processing them

--- rabbitmq-producer/src/main/java/com/course/rabbitmq/producer/producer/FixedRateProducer.java
+++ rabbitmq-producer/src/main/java/com/course/rabbitmq/producer/producer/FixedRateProducer.java
@@ -23,7 +23,7 @@ public class FixedRateProducer {
     @Scheduled(fixedRate = 500)
     public  void sendMessage() {
         i++;
-        LOG.info("i is " + i);
+//        LOG.info("i is " + i);
         rabbitTemplate.convertAndSend("course.fixedrate", "Fixed rate " + i);
     }
 }
