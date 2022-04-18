9c434f7a7c9823adff9df6fec44886e9a9314acc
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-04-18 10:23:55 +0100

    creates for FixedRateProducer for Fixed rate queue

---
+++ rabbitmq-producer/src/main/java/com/course/rabbitmq/producer/producer/FixedRateProducer.java
@@ -0,0 +1,29 @@
+package com.course.rabbitmq.producer.producer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FixedRateProducer {
+    private final RabbitTemplate rabbitTemplate;
+    
+    private static final Logger LOG = LoggerFactory.getLogger(FixedRateProducer.class);
+    
+    private int i = 0;
+    
+    public FixedRateProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+    
+    
+    @Scheduled(fixedRate = 500)
+    public  void sendMessage() {
+        i++;
+        LOG.info("i is " + i);
+        rabbitTemplate.convertAndSend("course.fixedrate", "Fixed rate " + i);
+    }
+}
