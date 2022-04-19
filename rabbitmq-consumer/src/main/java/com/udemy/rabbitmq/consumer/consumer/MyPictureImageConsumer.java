7f110a5fcd70a8c3a5902eb4c3e9a3a3c99ca34f
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-04-19 15:11:26 +0100

    update

--- rabbitmq-consumer/src/main/java/com/udemy/rabbitmq/consumer/consumer/MyPictureImageConsumer.java
+++ rabbitmq-consumer/src/main/java/com/udemy/rabbitmq/consumer/consumer/MyPictureImageConsumer.java
@@ -2,32 +2,41 @@ package com.udemy.rabbitmq.consumer.consumer;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rabbitmq.client.Channel;
 import com.udemy.rabbitmq.consumer.entity.Picture;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
 
-@Service
+import java.io.IOException;
+
+//@Service
 public class MyPictureImageConsumer {
     private static final Logger LOG = LoggerFactory.getLogger(MyPictureImageConsumer.class);
     
-    @Autowired
+//    @Autowired
     private ObjectMapper objectMapper;
     
     
     
     @RabbitListener(queues = "q.mypicture.image")
-    public void listen(String message) throws JsonProcessingException {
+    public void listen(String message, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag) throws IOException {
         Picture picture = objectMapper.readValue(message, Picture.class);
         
         if (picture.getSize() > 9000) {
-            throw new AmqpRejectAndDontRequeueException(picture + " Too large");
+//            throw new AmqpRejectAndDontRequeueException(picture + " Too large");
+            
+            channel.basicReject(tag,false);
+            return;
         }
         
         LOG.info("on Image queue {}", picture);
+        channel.basicAck(tag,false);
     }
     
 }
---
+++ rabbitmq-producer/src/main/java/com/course/rabbitmq/producer/producer/RetryPictureProducer.java
@@ -0,0 +1,23 @@
+package com.course.rabbitmq.producer.producer;
+
+import com.course.rabbitmq.producer.entity.Picture;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RetryPictureProducer {
+    
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+    
+    @Autowired
+    private ObjectMapper objectMapper;
+    
+    public void sendMessage(Picture picture) throws JsonProcessingException {
+        String json = objectMapper.writeValueAsString(picture);
+        rabbitTemplate.convertAndSend("x.guideline.work",picture.getType(), json);
+    }
+}
